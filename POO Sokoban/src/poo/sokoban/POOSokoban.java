fba5b3e93d2dd5e5cb84ffd3b344bbc8bbaf2f35
USUARIO <USUARIO@172.24.42.22>
2016-09-09 18:52:15 -0600

    Comentario

--- POO Sokoban/src/poo/sokoban/POOSokoban.java
+++ POO Sokoban/src/poo/sokoban/POOSokoban.java
@@ -1,11 +1,15 @@
-
 package poo.sokoban;
+
+
+import javax.swing.*;    
 
 public class POOSokoban {
 
 
     public static void main(String[] args) {
         System.out.print("Hola Mundo!");
+        JOptionPane.showMessageDialog(null,"Falta originalidad le doy un 6.5 xD");
+                
     }
     
 }
