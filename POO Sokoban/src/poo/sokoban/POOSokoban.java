eec3664db43c069e93040f975274fa59bd354908
Blad4542 <josecar366@gmail.com>
2016-09-23 19:28:40 -0600

    Cambios UML

--- POO Sokoban/src/poo/sokoban/POOSokoban.java
+++ POO Sokoban/src/poo/sokoban/POOSokoban.java
@@ -9,7 +9,7 @@ public class POOSokoban {
     public static void main(String[] args) {
         System.out.print("Hola Mundo!");
         JOptionPane.showMessageDialog(null,"Falta originalidad le doy un 6.5 xD");
-        // cambios minimos a las 3:11 PM
+        // cambios minimos a las 3:11
        System.out.println("\nSubido desde git bash");
     }
     
